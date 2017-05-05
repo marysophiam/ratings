c99e39784647cdacb1e84bd7cd6cdf0e948e6c19
vagrant <vagrant@vagrant.vm>
2017-05-05 06:36:33 +0000

    Did some cleaning up post-lab. Still very much in progress. Will complete as time allows. Lots to fix, and more to build still.

---
+++ model.py
@@ -0,0 +1,97 @@
+"""Models and database functions for Ratings project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class User(db.Model):
+    """User of ratings website."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    zipcode = db.Column(db.String(15), nullable=True)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<User user_id=%s email=%s>" % (self.user_id,
+                                               self.email)
+
+
+class Movie(db.Model):
+    """Movie information."""
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    # Modify here at some point to only display date (not inc. time) in table
+    released_at = db.Column(db.DateTime, nullable=True)
+    imdb_url = db.Column(db.String(1000), nullable=True)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Movie title=%s released_at=%s>" % (self.title,
+                                                    self.released_at)
+
+
+class Rating(db.Model):
+    """Rating for movies by user ID."""
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    movie_id = db.Column(db.Integer,
+                         db.ForeignKey('movies.movie_id'), nullable=False)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id'), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
+    # Define relationship to user
+    user = db.relationship("User", backref=db.backref("ratings",
+                                   order_by=rating_id))
+
+    # Define relationship to movie
+    movie = db.relationship("Movie", backref=db.backref("ratings",
+                                     order_by=rating_id))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        s = "<Rating rating_id=%s movie_id=%s user_id=%s score=%s>"
+        return s % (self.rating_id, self.movie_id, self.user_id,
+                    self.score)
+
+
+##############################################################################
+# Helper functions
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ratings'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
---
+++ server.py
@@ -0,0 +1,111 @@
+"""Movie Ratings."""
+
+from jinja2 import StrictUndefined
+
+from flask import (Flask, jsonify, render_template, redirect,
+                   request, flash, session)
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import User, Rating, Movie, connect_to_db, db
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails
+# silently. This is horrible. Fix this so that, instead, it raises an
+# error.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+    # a = jsonify([1, 3])
+    # return a
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route('/register', methods=['GET'])
+def register_form():
+    """Registration form."""
+
+    return render_template("register_form.html")
+
+
+@app.route('/register-submit', methods=['POST'])
+def register_process():
+    """Submit registration."""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # REPLACE THE FOLLOWING BLOCK WITH CLEANER, MORE PYTHONIC CODE
+
+    user = User.query.filter_by(email=email)
+    if user.all() == []:
+    #if not user....
+        user = User(email=email,
+                    password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash("You're now registered, great success!")
+    else:
+        flash("You're already registered.")
+
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET'])
+def login_form():
+    """Login form."""
+
+    return render_template("login_form.html")
+
+
+@app.route('/login-submit', methods=['POST'])
+def login_process():
+    """Process login."""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    # REPLACE THE FOLLOWING BLOCK WITH CLEANER, MORE PYTHONIC CODE
+
+    # At least this line is Pythonic now, need to flip conditional logic next
+    if not user:    
+        flash("There is no user with that email address in the system; please create an account.")
+        return redirect("/register")
+    else:
+        if password == user.password:
+            flash("You are logged in, great success!")
+            return redirect("/")
+        elif password != user.password:
+             flash("Password does not match email address; please try again.")
+             return redirect("/login")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    connect_to_db(app)
+
+    app.run(port=5000, host='0.0.0.0')
